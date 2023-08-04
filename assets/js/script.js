1b40995ac2b2af1cd6d95e54d41c668d4fdf18de
Gkicks <>
2023-08-04 09:55:09 +0000

    create array for generated cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
 // global variables
-
 const howToPlay = document.querySelector('#how-to-play');
 const howToPlayTitle = document.querySelector('#how-to-play-title');
 const instructions = document.querySelector('#instructions-div');
 const startGameButton = document.getElementById('start-button');
 const startGame = document.querySelector('#start-game-div-id');
 const input = document.querySelector('#enter-name');
-const gameCards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card');
 const cardFronts = document.getElementsByClassName('cardFront');
 const flippedCards = document.getElementsByClassName('flipCard');
 const toggledCards = document.getElementsByClassName('toggleCard');
+// an array for the cards to go in when generated
+let gameCards = [];
 // an array for the cards to go in when matched
 let matchedCards = [];
 const flipCount = document.getElementById('flips');
@@ -102,6 +103,7 @@ function generateCards() {
         cardBack.textContent = '?';
         cardBack.classList.add('cardBack');
         cards.appendChild(cardBack);
+        gameCards.push(cards);
     }
 }
 
@@ -113,7 +115,8 @@ generateCards();
  */
 function turnCard() {
     this.classList.toggle('toggleCard');
-};
+    console.log('turn working');
+}
 
 // https://linuxhint.com/add-class-to-clicked-element-using-javascript/
 // https://foolishdeveloper.com/how-to-play-sound-on-click-using-javascript/
@@ -122,186 +125,188 @@ function turnCard() {
  */
 function flipCard() {
     this.classList.add('flipCard');
-    audio = new Audio('assets/audio/card-flip-audio.mp3');
+    const audio = new Audio('assets/audio/card-flip-audio.mp3');
     audio.play();
-}
-
-/**
-* This function compares the names of the two flipped cards and check if they match
-*/
-function checkMatch() {
-    // function doesn't execute until it's checked that two cards have been flipped
-    if (flippedCards.length === 2) {
-
-        // checks if the name value of the two cards match
-        if (flippedCards[0].getAttribute('name') === flippedCards[1].getAttribute('name')) {
-
-            // stops the matched cards being able to be clicked again and pushed the cards into a matchedCards array
-            for (let card of flippedCards) {
-                card.style.pointerEvents = 'none';
-                matchedCards.push(card);
-            }
-
-            // removes the flipCard class for those cards that have been matched. 
-            // There is a setTimeout of 1.5 seconds as this is how long it takes the card to flip back. 
-            // This prevents the user from being able to click it again
-            flippedCards[0].classList.remove('flipCard');
-            flippedCards[0].classList.remove('flipCard');
-        } else {
-            // code if the two flipped cards don't match
-            // https://stackoverflow.com/questions/69300285/how-to-remove-a-class-with-delay-after-a-button-is-pressed
-            for (let card of flippedCards) {
-                // removes the toggleCard class so the card flip back over. One second timer set so the card still flips over before flipping back
-                setTimeout(function () {
-                    card.classList.remove('toggleCard');
-                }, 1000);
-            }
-
-            flippedCards[0].classList.remove('flipCard');
-            flippedCards[0].classList.remove('flipCard');
-        }
-    }
-}
-
-/**
- * Adds a class to cards if all are matched. This gives an animation and then a winning page 
- */
-function wonGame() {
-    // checks how many cards are in the matchedCards array. If this equals the number of cards in game the user has won
-    if (matchedCards.length === gameCards.length) {
-        // delays animation by half a second to improve user experience
-        setTimeout(function () {
-            // adds the won-game-bears class which causes the cards to have a wiggle animation
-            // const cardFronts = document.getElementsByClassName('cardFront');
-            for (let card of cardFronts) {
-                card.classList.add('won-game-bears');
-            }
-        }, 500);
-
-        // Congratulation page that appears two seconds after user has won the game. The delay is so the user will see the wiggle animation
-        setTimeout(function () {
-            const winningPage = document.getElementById('won-game');
-            const winningPageHeading = document.querySelector('#winning-title');
-            const timeCount = document.getElementById('time');
-            const timeRemaining = timeCount.textContent;
-            const flipCount = document.getElementById('flips');
-            const flipsTaken = flipCount.textContent;
-            winningPage.classList.remove('won-game-hidden');
-            winningPage.classList.add('won-game-show');
-            winningPageHeading.textContent = `Congratulations ${input.value}! You found all the matching bears in 
-                    ${flipsTaken} flips and with ${timeRemaining} seconds remaining. Press below to start a new game:`;
-        }, 2000);
-
-        // button on winning page to refresh the browser and restart the game
-        button = document.getElementById('restart-won');
-        button.addEventListener('click', function () { location.reload(); });
-    }
+    console.log('flip working');
 }
 
 for (let card of gameCards) {
     card.addEventListener('click', turnCard);
     card.addEventListener('click', flipCard);
-    card.addEventListener('click', checkMatch);
-    card.addEventListener('click', wonGame);
-    card.addEventListener('click', countFlip);
-    card.addEventListener('click', disableGame);
+    // card.addEventListener('click', checkMatch);
+    // card.addEventListener('click', wonGame);
+    // card.addEventListener('click', countFlip);
+    // card.addEventListener('click', disableGame);
 }
 
-/**
- * This function disables the event listeners while there are two unmatched cards flipped
- */
-function disableGame() {
-    // this needs to be a formula as the toggleClass class still shows when the cards are matched. This ensures it's only the number of  
-    if (toggledCards.length - matchedCards.length >= 2) {
-        for (let card of gameCards) {
-            card.removeEventListener('click', turnCard);
-            card.removeEventListener('click', flipCard);
-            card.removeEventListener('click', checkMatch);
-            card.removeEventListener('click', wonGame);
-            card.removeEventListener('click', countFlip);
-        }
-    } else {
-        for (let card of gameCards) {
-            card.addEventListener('click', turnCard);
-            card.addEventListener('click', flipCard);
-            card.addEventListener('click', checkMatch);
-            card.addEventListener('click', wonGame);
-            card.addEventListener('click', countFlip);
-            card.addEventListener('click', disableGame);
-        }
-    }
-}
+// /**
+// * This function compares the names of the two flipped cards and check if they match
+// */
+// function checkMatch() {
+//     // function doesn't execute until it's checked that two cards have been flipped
+//     if (flippedCards.length === 2) {
 
-setInterval(disableGame, 500);
+//         // checks if the name value of the two cards match
+//         if (flippedCards[0].getAttribute('name') === flippedCards[1].getAttribute('name')) {
 
-function countFlip() {
-    flipCount.textContent = flips + 1;
-    flips = parseInt(flipCount.textContent);
-}
+//             // stops the matched cards being able to be clicked again and pushed the cards into a matchedCards array
+//             for (let card of flippedCards) {
+//                 card.style.pointerEvents = 'none';
+//                 matchedCards.push(card);
+//             }
 
-// create function that gives different time limits depending on the difficulty selected
-const timeCount = document.getElementById('time');
-let time = '100';
-timeCount.textContent = time;
-const selectDifficulty = document.getElementById('select-difficulty');
+//             // removes the flipCard class for those cards that have been matched. 
+//             // There is a setTimeout of 1.5 seconds as this is how long it takes the card to flip back. 
+//             // This prevents the user from being able to click it again
+//             flippedCards[0].classList.remove('flipCard');
+//             flippedCards[0].classList.remove('flipCard');
+//         } else {
+//             // code if the two flipped cards don't match
+//             // https://stackoverflow.com/questions/69300285/how-to-remove-a-class-with-delay-after-a-button-is-pressed
+//             for (let card of flippedCards) {
+//                 // removes the toggleCard class so the card flip back over. One second timer set so the card still flips over before flipping back
+//                 setTimeout(function () {
+//                     card.classList.remove('toggleCard');
+//                 }, 1000);
+//             }
 
-selectDifficulty.addEventListener('change', function difficulty() {
-    if (selectDifficulty.value === 'easy') {
-        let time = '100';
-        timeCount.textContent = time;
-    } else if (selectDifficulty.value === 'medium') {
-        let time = '60';
-        timeCount.textContent = time;
-    } else {
-        let time = '5';
-        timeCount.textContent = time;
-    }
-});
+//             flippedCards[0].classList.remove('flipCard');
+//             flippedCards[0].classList.remove('flipCard');
+//         }
+//     }
+// }
 
-/**
- * This function decreases the timer by one every second
- */
-function timeGame() {
-    let currentTime = timeCount.innerHTML;
-    currentTime--;
-    timeCount.innerHTML = currentTime;
-    setTimeout(timeGame, 1000);
-    for (let card of gameCards) {
-        card.removeEventListener('click', timeGame);
-    }
-}
+// /**
+//  * Adds a class to cards if all are matched. This gives an animation and then a winning page 
+//  */
+// function wonGame() {
+//     // checks how many cards are in the matchedCards array. If this equals the number of cards in game the user has won
+//     if (matchedCards.length === cards.length) {
+//         // delays animation by half a second to improve user experience
+//         setTimeout(function () {
+//             // adds the won-game-bears class which causes the cards to have a wiggle animation
+//             // const cardFronts = document.getElementsByClassName('cardFront');
+//             for (let card of cardFronts) {
+//                 card.classList.add('won-game-bears');
+//             }
+//         }, 500);
 
-for (let card of gameCards) {
-    card.addEventListener('click', timeGame);
-}
+//         // Congratulation page that appears two seconds after user has won the game. The delay is so the user will see the wiggle animation
+//         setTimeout(function () {
+//             const winningPage = document.getElementById('won-game');
+//             const winningPageHeading = document.querySelector('#winning-title');
+//             const timeCount = document.getElementById('time');
+//             const timeRemaining = timeCount.textContent;
+//             const flipCount = document.getElementById('flips');
+//             const flipsTaken = flipCount.textContent;
+//             winningPage.classList.remove('won-game-hidden');
+//             winningPage.classList.add('won-game-show');
+//             winningPageHeading.textContent = `Congratulations ${input.value}! You found all the matching bears in 
+//                     ${flipsTaken} flips and with ${timeRemaining} seconds remaining. Press below to start a new game:`;
+//         }, 2000);
 
-/**
- * This function checks, each second, if the timer has reached zero. If so it will 
- * display a lost game page
- */
-function lostGame() {
-    // check to see if the timer reaches zero
-    if (timeCount.textContent === '0') {
-        // losing banner to appear
-        const losingBanner = document.getElementById('lost-game');
-        const losingBannerHeading = document.querySelector('#losing-title');
-        losingBanner.classList.remove('lost-game-hidden');
-        losingBanner.classList.add('lost-game-show');
-        // button on losing page to refresh the browser and restart the game
-        const button = document.getElementById('restart-lost');
-        button.addEventListener('click', function () { location.reload(); });
-        losingBannerHeading.textContent = `Sorry ${input.value} you didn't match all the bears in time! 
-                 Press below to start a new game:`;
-    }
-}
+//         // button on winning page to refresh the browser and restart the game
+//         button = document.getElementById('restart-won');
+//         button.addEventListener('click', function () { location.reload(); });
+//     }
+// }
 
-setInterval(lostGame, 100);
 
-//button to give the user the opportunity to restart the game at any time
-const restartButton = document.getElementById('restart-game');
-restartButton.addEventListener('click', function () { location.reload(); });
+// /**
+//  * This function disables the event listeners while there are two unmatched cards flipped
+//  */
+// function disableGame() {
+//     // this needs to be a formula as the toggleClass class still shows when the cards are matched. This ensures it's only the number of  
+//     if (toggledCards.length - matchedCards.length >= 2) {
+//         for (let card of cards) {
+//             card.removeEventListener('click', turnCard);
+//             card.removeEventListener('click', flipCard);
+//             card.removeEventListener('click', checkMatch);
+//             card.removeEventListener('click', wonGame);
+//             card.removeEventListener('click', countFlip);
+//         }
+//     } else {
+//         for (let card of cards) {
+//             card.addEventListener('click', turnCard);
+//             card.addEventListener('click', flipCard);
+//             card.addEventListener('click', checkMatch);
+//             card.addEventListener('click', wonGame);
+//             card.addEventListener('click', countFlip);
+//             card.addEventListener('click', disableGame);
+//         }
+//     }
+// }
 
-// add event default to click functions?
-// add sound?
-// make the website responsive
-// add 404 page
+// setInterval(disableGame, 500);
+
+// function countFlip() {
+//     flipCount.textContent = flips + 1;
+//     flips = parseInt(flipCount.textContent);
+// }
+
+// // create function that gives different time limits depending on the difficulty selected
+// const timeCount = document.getElementById('time');
+// let time = '100';
+// timeCount.textContent = time;
+// const selectDifficulty = document.getElementById('select-difficulty');
+
+// selectDifficulty.addEventListener('change', function difficulty() {
+//     if (selectDifficulty.value === 'easy') {
+//         let time = '100';
+//         timeCount.textContent = time;
+//     } else if (selectDifficulty.value === 'medium') {
+//         let time = '60';
+//         timeCount.textContent = time;
+//     } else {
+//         let time = '5';
+//         timeCount.textContent = time;
+//     }
+// });
+
+// /**
+//  * This function decreases the timer by one every second
+//  */
+// function timeGame() {
+//     let currentTime = timeCount.innerHTML;
+//     currentTime--;
+//     timeCount.innerHTML = currentTime;
+//     setTimeout(timeGame, 1000);
+//     for (let card of gameCards) {
+//         card.removeEventListener('click', timeGame);
+//     }
+// }
+
+// for (let card of cards) {
+//     card.addEventListener('click', timeGame);
+// }
+
+// /**
+//  * This function checks, each second, if the timer has reached zero. If so it will 
+//  * display a lost game page
+//  */
+// function lostGame() {
+//     // check to see if the timer reaches zero
+//     if (timeCount.textContent === '0') {
+//         // losing banner to appear
+//         const losingBanner = document.getElementById('lost-game');
+//         const losingBannerHeading = document.querySelector('#losing-title');
+//         losingBanner.classList.remove('lost-game-hidden');
+//         losingBanner.classList.add('lost-game-show');
+//         // button on losing page to refresh the browser and restart the game
+//         const button = document.getElementById('restart-lost');
+//         button.addEventListener('click', function () { location.reload(); });
+//         losingBannerHeading.textContent = `Sorry ${input.value} you didn't match all the bears in time! 
+//                  Press below to start a new game:`;
+//     }
+// }
+
+// setInterval(lostGame, 100);
+
+// //button to give the user the opportunity to restart the game at any time
+// const restartButton = document.getElementById('restart-game');
+// restartButton.addEventListener('click', function () { location.reload(); });
+
+// // add event default to click functions?
+// // add sound?
+// // make the website responsive
+// // add 404 page
